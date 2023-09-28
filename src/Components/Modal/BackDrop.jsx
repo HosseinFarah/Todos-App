47c207d061dce6c18547c780bda52df6b97ff34a
Hossein Farahkordmahaleh <113180290+HosseinFarah@users.noreply.github.com>
2023-09-28 11:53:52 +0300

    .

--- src/Components/Modal/BackDrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./backdrop.css";
-const BackDrop = (props) => {
-  return (
-    props.show && <div className="bg" onClick={props.hideLoginHandler}></div>
-  )
-};
-export default React.memo(BackDrop);
